4f7810fcbd28fd04cae3fc9ae486a248280f33c3
Reno McKenzie <Reno@RenoMcKenzie.com>
2018-04-25 21:37:46 -0700

    added a log of functionality

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const shell = require('shelljs');
-const child_process = require('child_process');
 const LogScale = require('log-scale');
 
 const device = '/dev/cu.usbmodem14541';
 const logScale = new LogScale(0, 8);
-const maxPing = 2000;
+const maxPing = 1000;
 
 // -------------------------------------------------------------------------- //
 
@@ -18,16 +17,18 @@ class DisplayMatrix {
       COLOR: '\\xD0',
     }
 
-    this.BLOCKS = [' ', '▁','▂','▃','▄','▅','▆','▇','█'];
+    // downtime components
+    this.uptime = Date.now();
+    this.downtime = null;
+
+    // graph components
+    this.BLOCKS = ['_', '▁','▂','▃','▄','▅','▆','▇','█'];
     this.graph = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    this.lastEntry = 0;
 
-    setInterval(() => {
-      const rand = parseInt(Math.random() * 8);
-      this.graph.push(rand);
-      this.graph.shift();
-
-      this.mockGraph()
-    }, 1000);
+    // setInterval components
+    this.debounce = false;
+    this.clock = null;
   }
 
   static linearToLog(ms) {
@@ -38,11 +39,16 @@ class DisplayMatrix {
 
   static ping(callback) {
     if (!callback) return;
-    let result = null;
+    let result = maxPing;
 
     shell.exec('ping -c 1 -t 10 google.com | grep "time="', { silent: true }, (code, stdout, stderr) => {
-      if (code || stderr) return;
+      // timeout or error
+      if (code) {
+        callback(0 - code);
+        return;
+      }
 
+      // successful ping
       const match = stdout.match(/time=(\d+\.\d+)\s(\w{2})/);
       if (match) {
         const unit = match[2];
@@ -55,6 +61,45 @@ class DisplayMatrix {
     });
   }
 
+  static textAlign(direction, base, str) {
+    let newStr = str.slice(0,7);
+    let newBase;
+    let result = '';
+
+    if (direction === 'right') {
+      newBase = base.slice(0, 16 - newStr.length);
+      result = newBase + newStr;
+    } else {
+      newBase = base.slice(8);
+      result = newStr + ' ' + newBase;
+    }
+
+    while (result.length < 16) result = ' ' + result;
+    return result;
+  }
+
+  static formatTime(seconds) {
+    const time = {
+      d: seconds / 60 / 60 / 24 | 0,
+      h: seconds / 60 / 60 % 24 | 0,
+      m: seconds / 60 % 60 | 0,
+      s: seconds % 60,
+    };
+
+    const pad = int => `${int}`.length < 2 ? `0${int}` : `${int}`;
+    const append = (base, numStr) => base.length + numStr.length <= 8 ? base + numStr : base;
+
+    let final = '';
+    Object.keys(time).forEach(unit => {
+      if (!time[unit]) return;
+      let timeUnitStr = time[unit];
+      if (unit !== 'd') timeUnitStr = pad(time[unit]);
+      final = append(final, `${timeUnitStr}${unit}`);
+    });
+
+    return final;
+  }
+
   command(cmd, params) {
     const str = this.CODES.PREFIX + cmd + (params || []).join('');
     shell.exec(`echo "${str}" > ${device}`, (code, stdout, stderr) => {
@@ -64,12 +109,65 @@ class DisplayMatrix {
     });
   }
 
-  mockGraph() {
+  plotGraph() {
     let str = '';
-    this.graph.forEach(int => {
-      str += this.BLOCKS[int];
-    });
-    console.log(str);
+    this.graph.forEach(int => { str += this.BLOCKS[int] });
+    return str;
+  }
+
+  updateScreen() {
+    const graph = this.plotGraph();
+
+    let scheme = '';
+    if (this.downtime) {
+      const down = parseInt((Date.now() - this.downtime) / 1000);
+      scheme = this.constructor.textAlign('right', 'Downtime:       ', `${down}s`);
+    }
+    else {
+      const up = parseInt((Date.now() - this.uptime) / 1000);
+      scheme = this.constructor.textAlign('middle', '        |       ', `${this.lastEntry}ms`);
+      scheme = this.constructor.textAlign('right', scheme, `${up}s`);
+      // scheme = `${this.lastEntry} ms | ${up} sec`;
+    }
+    console.log(scheme);
+    console.log(`[${graph}]`);
+    console.log('')
+  }
+
+  interpret(ms) {
+    // if ping returned with 'no reply' or 'other errors'
+    if (ms < 0) {
+      ms = maxPing; // don't update parameter
+      this.downtime = this.downtime || Date.now();
+      this.uptime = null;
+    } else if (this.downtime) {
+      this.downtime = null;
+      this.uptime = Date.now();
+    }
+
+    const barLevel = this.constructor.linearToLog(ms); // 0-8
+    this.graph.push(barLevel);
+    this.graph.shift();
+    this.lastEntry = ms;
+
+    this.updateScreen();
+  }
+
+  start(interval) {
+    const newInterval = interval < 1000 ? 1000 : interval;
+    this.clock = setInterval(() => {
+      if (this.debounce) return;
+      this.debounce = true;
+
+      this.constructor.ping(ms => {
+        this.interpret(ms);
+        this.debounce = false;
+      });
+    }, newInterval);
+  }
+
+  stop() {
+    clearInterval(this.clock);
   }
 
 }
@@ -77,4 +175,4 @@ class DisplayMatrix {
 // -------------------------------------------------------------------------- //
 
 const display = new DisplayMatrix();
-
+display.start(1000);
