6742b790455c811354629e8d821cfee4450dae66
Reno McKenzie <Reno@RenoMcKenzie.com>
2018-05-11 22:06:54 -0700

    fix for pi demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 const DisplayMatrix = require('./');
 
 const display = new DisplayMatrix();
-display.device = '/dev/cu.usbmodem14541';
+display.device = '/dev/ttyACM0'; // what it shows up on the Pi.
 display.start(15000);
